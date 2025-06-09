66267f6c966f8109bcf2a01e029174c3be7f9bc2
lisboa3 <isabelly.lisboa.silva@escola.pr.gov.br>
2025-06-09 09:42:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'Animais Marinhos',
+    'Qual o maior animal marinho do mundo?',
+    'O maior animal marinho do mundo é a baleia azul e ela pode medir até 30 metros de comprimento e pesar mais de 200 toneladas.'
+)
+
+criaCartao(
+    'Animais Terrestres',
+    'Qual o animal terrestre mais rápido do mundo?',
+    'O animal terrestre mais rápido do mundo é o guepardo,també, conhecido como chita.Ele pode chegar até 120km/h.'
+)
+
+criaCartao(
+    'Animais Aéreos ',
+    'Por que aves não possuem bexigas?',
+    'As aves não têm bexiga urinária porque é uma adaptação que as ajuda a voar. Elas precisam voare o peso de um órgão cheio de líquido dentro do corpo atrapalharia essa função.'
+     ,
+
+)
+
+criaCartao(
+    'Animais anfíbios',
+    'Quais as principais características dos animais anfíbios?',
+    'Sua característica mais marcantes é a presença de um ciclo de vida com uma fase larval aquática e uma fase adulta terrestre, além dos anfíbios serem ectotérmicos, ou seja, não são capazes de regular a temperatura corporal por mecanismos internos.',
+    
+)
